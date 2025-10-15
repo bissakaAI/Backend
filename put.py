fdd70d9bed8769728c2aa0a29ff369aeb4d53692
bissakaAI <bissakabissakadop31.com>
2025-10-15 09:32:23 +0100

    putdel

---
+++ put.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler,HTTPServer
+import json
+
+data = [{
+    'name':'Sam larry',
+    'track':'Ai Engineer'
+}]
+
+
+class BaseAPI(BaseHTTPRequestHandler):
+    def send_data(self,payload,status=200):
+        self.send_response(status)
+        self.send_header('content-type','application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(payload).encode())
+
+    def do_PUT(self):
+        content_size = int(self.headers.get('content-Length',0))
+        raw_data=self.rfile.read(content_size)
+        new_data=json.loads(raw_data.decode())
+        for i in data:
+            if i['name']== new_data['name']:
+                i.update(new_data) #like saving to a database
+        
+def run():
+    HTTPServer(('localhost',8000),BaseAPI).serve_forever()
+
+
+print('Application is running')
+run()
