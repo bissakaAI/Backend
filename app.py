2c57db2063cbeaa6f26841fd82eaef9aa395f0ef
BissakaAi <bissakabissakadop31@gmail.com>
2025-10-16 12:47:21 +0100

    fastapi

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from dotenv import 
+from dotenv import load_dotenv
 import os
+import uvicorn
+
+load_dotenv()
+
+
+app = FastAPI(title='Simple FastAPI App',version='1.0.0')
+
+#decorate the root function
+@app.get("/") #endpoint is the root which is the forward slash
+def root():
+    return {'Message':'Welcome to my FastAPI Application'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app,host=os.getenv("host"),port=int(os.getenv("port")))
