a49a46bb306bc630b079fbefba69b31e3318695c
bissakaAI <bissakabissakadop31.com>
2025-10-21 10:25:25 +0100

    Tuesday

---
+++ FastAPI/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine,text
+from sqlalchemy.orm import sessionmaker                                                                                  
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_url = f'mysql+pymysql://{os.getenv("dbuser")}:{os.getenv("dbpassword")}@{os.getenv("dbhost")}:{os.getenv("dbport")}/{os.getenv("dbname")}'
+engine = create_engine(db_url)
+session=sessionmaker(bind=engine)
+db=session()
+
+query= text("SELECT * FROM user")
+userd=db.execute(query).fetchall()
+print(userd)
